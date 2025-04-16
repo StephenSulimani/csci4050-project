72f8508049841780c6b559f824f5385bd916a216
StephenSulimani <stephensulimani@gmail.com>
2025-04-16 19:18:50 -0400

    :tada: added /api/me GET

---
+++ src/app/api/me/route.ts
@@ -0,0 +1,38 @@
+import User from "@/db/models/User";
+import { NextRequest, NextResponse } from "next/server";
+
+export const GET = async (req: NextRequest) => {
+    const user_id = req.headers.get('x-user-id')
+
+    if (!user_id) {
+        return NextResponse.json({
+            status: 0,
+            error: 1,
+            message: "You need to be authenticated"
+        }, { status: 401 })
+    }
+
+    const user = await User.findOne({
+        where: {
+            id: user_id
+        }
+    })
+
+    if (!user) {
+        return NextResponse.json({
+            status: 0,
+            error: 1,
+            message: "User not found"
+        }, { status: 404 })
+    }
+
+    return NextResponse.json({
+        status: 1,
+        error: 0,
+        message: {
+            id: user.dataValues.id,
+            name: user.dataValues.name,
+            email: user.dataValues.email
+        }
+    })
+}
