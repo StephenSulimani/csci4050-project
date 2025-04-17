9a0c4ced7ca0281a067637567548b41a3462c480
StephenSulimani <stephensulimani@gmail.com>
2025-04-17 14:19:12 -0400

    :hammer: /portfolio now displays all portfolios with their value

--- src/app/api/portfolio/route.ts
+++ src/app/api/portfolio/route.ts
@@ -1,4 +1,6 @@
+import { FinnhubUtils } from "@/app/FinnhubUtils";
 import { connect } from "@/db/connection";
+import Order from "@/db/models/Order";
 import Portfolio from "@/db/models/Portfolio";
 import { NextRequest, NextResponse } from "next/server";
 
@@ -6,6 +8,13 @@ interface IPortfolioCreationRequest {
     name?: string;
     starting_capital?: number;
 }
+
+type Holding = {
+    ticker: string,
+    amount: number,
+    value: number
+}
+
 
 export const GET = async (req: NextRequest) => {
     const user_id = req.headers.get('x-user-id');
@@ -27,23 +36,36 @@ export const GET = async (req: NextRequest) => {
             }
         })
 
+        const portfolios_json = []
+
+        for (const portfolio of portfolios) {
+
+            const cash = await FinnhubUtils.calcCash(portfolio.dataValues.id);
+            const stocks = await getStocks(portfolio.dataValues.id);
+            const total_value = await getTotalValue(cash, stocks);
+            portfolios_json.push({
+                "name": portfolio.dataValues.name,
+                "id": portfolio.dataValues.id,
+                "starting_capital": portfolio.dataValues.startingCapital,
+                "cash": cash,
+                "stocks": stocks,
+                "total_value": total_value
+            })
+
+        }
+
         const response_json = {
             "status": 1,
             "error": 0,
             "message": {
-                "portfolios": portfolios.map((portfolio) => {
-                    return {
-                        "name": portfolio.dataValues.name,
-                        "id": portfolio.dataValues.id,
-                        "starting_capital": portfolio.dataValues.startingCapital
-                    }
-                })
+                portfolios: portfolios_json
             }
         }
 
         return NextResponse.json(response_json, { status: 200 })
     }
-    catch {
+    catch (e) {
+        console.log(e)
         return NextResponse.json({
             status: 0,
             error: 1,
@@ -86,7 +108,6 @@ export const PUT = async (req: NextRequest) => {
         })
     }
     catch (e) {
-        console.log(e);
         return NextResponse.json({
             status: 0,
             error: 1,
@@ -95,3 +116,44 @@ export const PUT = async (req: NextRequest) => {
     }
 
 }
+
+const getTotalValue = async (cash: number, stocks: Holding[]) => {
+    let unrealized_gains = 0;
+    for (const stock of stocks) {
+        unrealized_gains += stock.value;
+    }
+    return cash + unrealized_gains;
+}
+
+const getStocks = async (portfolio_id: string): Promise<Holding[]> => {
+    const orders = await Order.findAll({
+        where: {
+            portfolio_id: portfolio_id
+        }
+    });
+    const stocks = {};
+    for (let order of orders) {
+        order = order.dataValues;
+        if (stocks[order.ticker] && order.type === 'BUY') {
+            stocks[order.ticker] += order.amount;
+        } else if (stocks[order.ticker] && order.type === 'SELL') {
+            stocks[order.ticker] -= order.amount;
+        } else if (order.type === 'BUY') {
+            stocks[order.ticker] = order.amount;
+        } else if (order.type === 'SELL') {
+            stocks[order.ticker] = -order.amount;
+        }
+    }
+
+    const stock_array: Holding[] = await Promise.all(Object.entries(stocks).map(async ([ticker, amount]) => {
+        return {
+            "ticker": ticker,
+            "amount": amount,
+            "value": (await FinnhubUtils.getPrice(ticker)) * amount,
+        }
+    }));
+
+    return stock_array;
+}
+
+
